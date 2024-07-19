f34cc4795291345e318af76402cec5176fca3459
Mike Zupper <me@mikezupper.com>
2024-07-19 10:41:50 -0400

    free cuda memory after each job

--- runner/app/pipelines/base.py
+++ runner/app/pipelines/base.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Any
+import logging
+import torch
 
+logger = logging.getLogger(__name__)
 
 class Pipeline(ABC):
     @abstractmethod
@@ -10,3 +13,9 @@ class Pipeline(ABC):
     @abstractmethod
     def __call__(self, inputs: Any) -> Any:
         raise NotImplementedError("Pipeline should implement a __call__ method")
+
+    def print_gpu_memory_usage(self):
+        allocated = torch.cuda.memory_allocated() / (1024**3)
+        cached = torch.cuda.memory_reserved() / (1024**3)
+        logger.info(f"Allocated memory: {allocated:.2f} GB")
+        logger.info(f"Cached memory: {cached:.2f} GB")
