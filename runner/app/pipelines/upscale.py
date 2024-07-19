1779b0f9e830ad1f2c90daee8bf08acbc03afdd1
Mike Zupper <me@mikezupper.com>
2024-07-19 10:41:50 -0400

    free cuda memory after each job

--- runner/app/pipelines/upscale.py
+++ runner/app/pipelines/upscale.py
@@ -120,6 +120,9 @@ class UpscalePipeline(Pipeline):
         else:
             has_nsfw_concept = [None] * len(output.images)
 
+        self.print_gpu_memory_usage()
+        torch.cuda.empty_cache()
+        self.print_gpu_memory_usage()
         return output.images, has_nsfw_concept
 
     def __str__(self) -> str:
